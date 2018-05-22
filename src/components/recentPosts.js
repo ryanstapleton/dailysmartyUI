94e9fddcb5efb850d336210354127b73f428526f
ryanstapleton <megalixir.1@gmail.com>
2018-05-21 23:45:56 -0600

    Added recent posts component boilerplate

---
+++ src/components/recentPosts.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class RecentPosts extends Component {
+  render() {
+    return (
+      <div className="recent-posts">
+        <div className="recent-posts__wrapper">
+          <div className="recent-posts__heading">Recent Posts</div>
+          <ul className="recent-posts__posts">
+            <li>Recent Post 0</li>
+            <li>Recent Post 1</li>
+            <li>Recent Post 2</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RecentPosts;

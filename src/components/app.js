83609a6cdce5e4c659177543056d2b0a7ff1f3dc
ryanstapleton <megalixir.1@gmail.com>
2018-05-21 20:45:03 -0600

    title, initial data confirm

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <div>
+          logo
+        </div>
       </div>
     );
   }
